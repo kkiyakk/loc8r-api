52010476caa85c26c29b37a0d68d5c5f525e50a9
kkiyakk <166090798+kkiyakk@users.noreply.github.com>
2025-11-26 01:52:05 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// require('./app_server/models/db');
 require('./app_api/models/db');
 
-//const indexRouter = require('./app_server/routes/about');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
 const apiRouter = require('./app_api/routes/index');
+var app = express();
+
 const cors = require('cors');
+const corsOptions = {
+  origin: 'https://loc8r-netlify-app.netlify.app',
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+app.use(cors(corsOptions));
 
-const app = express();
-
+// view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
@@ -19,28 +27,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'app_public', 'build/browser')));
-app.use(express.static(path.join(__dirname, 'app_public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({
-  origin: 'http://localhost:4200',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Origin','X-Requested-With','Content-Type','Accept']
-}));
+// app.use('/api', (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept, Authorization");
+//   next();
+// });
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-//app.use('/', indexRouter);
 
-
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 module.exports = app;
+
+
