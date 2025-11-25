87a8d392f831a4662880211d18d5dac596c8ab2e
kkiyakk <166090798+kkiyakk@users.noreply.github.com>
2025-11-26 02:20:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 // require('./app_server/models/db');
 require('./app_api/models/db');
 
-const indexRouter = require('./app_server/routes/about');
+const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const apiRouter = require('./app_api/routes/index');
 var app = express();
